9a23c9bb6743e8e7d5ce286a4fe38f7da14a117a
MikeTheMater <mmparmpas@gmail.com>
2026-05-21 00:53:51 +0300

    Added some products to visualize and working on products page

--- store/src/main/java/com/example/store/product/ProductController.java
+++ store/src/main/java/com/example/store/product/ProductController.java
@@ -28,7 +28,7 @@ public class ProductController {
     @GetMapping
     public Page<Product> getAll(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "20") int size,
             @RequestParam(defaultValue = "id") String sortBy,
             @RequestParam(required = false) String name
     ) {
